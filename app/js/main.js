6a545ce756ce1e732cde7cc5b5f04f87e2cc72a9
rydmax18 <rydzanych18@gmail.com>
2021-08-05 20:10:51 +0300

    finished homepage

--- app/js/main.js
+++ app/js/main.js
@@ -15,6 +15,16 @@ $(function(){
     
   });
 
-  let mixer = mixitup('.top-goods__content');
+  let containerEl1 = document.querySelector('[data-ref="container-1"]');
+  let containerEl2 = document.querySelector('[data-ref="container-2"]');
+
+  let config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+
+  let mixer1 = mixitup(containerEl1, config);
+  let mixer2 = mixitup(containerEl2, config);
 
 });
